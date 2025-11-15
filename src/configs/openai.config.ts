cc6de079f3e9df1e8751a752c96f031ebfd7db9f
ranielART <ranielmontebon24@gmail.com>
2025-11-15 14:01:18 +0800

    chore: add dotenv and openapi config files

---
+++ src/configs/openai.config.ts
@@ -0,0 +1,8 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+        baseURL: 'https://api.deepseek.com',
+        apiKey: process.env.DEEPSEEK_API_KEY,
+});
+
+export default openai;
