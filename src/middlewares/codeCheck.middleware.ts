0c0bb6c0a8c4dec1e964965777b10576c8f73161
ranielART <ranielmontebon24@gmail.com>
2025-11-20 15:48:07 +0800

    fix: Changed the error for code checking to only UNAUTHORIZED!

--- src/middlewares/codeCheck.middleware.ts
+++ src/middlewares/codeCheck.middleware.ts
@@ -4,7 +4,7 @@ export const codeCheck = (req: Request, res: Response, next: NextFunction) => {
   const { code } = req.query;
 
   if (!code || typeof code !== "string") {
-    const error = new Error("Access code is required.") as CustomError;
+    const error = new Error("Unauthorized!") as CustomError;
     error.statusCode = 400;
     return next(error);
   }
@@ -17,7 +17,7 @@ export const codeCheck = (req: Request, res: Response, next: NextFunction) => {
   }
 
   if (code !== envCode) {
-    const error = new Error("Unauthorized. Please enter a valid access code.") as CustomError;
+    const error = new Error("Unauthorized!") as CustomError;
     error.statusCode = 401;
     return next(error);
   }
